dda2704b6fd217b13f4e73a051dcd00548feec58
Frm2003 <felipper475@gmail.com>
2024-02-25 15:54:15 -0300

    update main

--- src/main/java/com/br/fatec/AGIS/controller/CursoController.java
+++ src/main/java/com/br/fatec/AGIS/controller/CursoController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,8 +21,7 @@ import com.br.fatec.AGIS.service.CursoService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/curso")
-@CrossOrigin("http://localhost:3000")
+@RequestMapping("/cursos")
 public class CursoController {
 	@Autowired
 	private CursoService cursoService;
--- src/main/java/com/br/fatec/AGIS/controller/GradeCurricularController.java
+++ src/main/java/com/br/fatec/AGIS/controller/GradeCurricularController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,8 +19,7 @@ import com.br.fatec.AGIS.service.GradeCurricularService;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/gradeCurricular")
-@CrossOrigin("http://localhost:3000")
+@RequestMapping("/grades")
 public class GradeCurricularController {
 	@Autowired
 	private GradeCurricularService gradeCurricularService;
